bbcb22df5bc316ae1d339fcb24794c553ad4fd5b
ageht <Vladimir.E.Pavlov@yandex.ru>
2022-08-02 21:45:39 +0700

    new files 12,13

---
+++ SkyPro_Python/7_1/12.py
@@ -0,0 +1,17 @@
+ponds = [
+    {"pk": 1, "volume": 10000, "fish": "тунец"},
+    {"pk": 192, "volume": 20000, "fish": "морская камбала"},
+    {"pk": 206, "volume": 10000, "fish": "треска"},
+    {"pk": 322, "volume": 25000, "fish": "тунец"},
+    {"pk": 420, "volume": 20000, "fish": "морская камбала"},
+    {"pk": 704, "volume": 10000, "fish": "треска"},
+    {"pk": 920, "volume": 25000, "fish": "тунец"},
+]
+for i in range(len(ponds)):
+    if ponds[i]["fish"] == "тунец":
+        del (ponds[i])
+
+# Не удаляйте код ниже, это код проверки
+
+for pond in ponds:
+    print(pond["pk"])
