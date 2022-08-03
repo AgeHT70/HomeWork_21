22991fc6821778fefa29e980e433ec0c0bbc6080
ageht <Vladimir.E.Pavlov@yandex.ru>
2022-08-03 22:30:05 +0700

    write functions start main

---
+++ SkyPro_Python/7_2/HomeWork/functions.py
@@ -0,0 +1,74 @@
+from os import path
+import json
+
+
+def load_students(filename=path.join("Data", "students.json")) -> dict:
+    """
+    Load information about students from file
+    :param filename: name of file with information about students
+    :return: dictionary  with information about students
+    """
+    with open(filename) as students_file:
+        students = json.load(students_file)
+    return students
+
+
+def load_professions(filename=path.join("Data", "professions.json")):
+    """
+    Load information about professions from file
+    :param filename: name of file with information about professions
+    :return: dictionary  with information about professions
+    """
+    with open(filename) as professions_file:
+        professions = json.load(professions_file)
+    return professions
+
+
+def get_student_by_pk(pk: int) -> dict | str:
+    """
+    Get dict with data about student by pk
+    :param pk: number of student
+    :return: dictionary with data about student
+    """
+    students = load_students()
+    for student in students:
+        if student["pk"] == pk:
+            return student
+    return "У нас нет такого студента"
+
+
+def get_profession_by_title(title: str) -> dict | str:
+    """
+    Get dictionary with data about profession by title
+    :param title: name of profession
+    :return: dictionary with data about profession
+    """
+    professions = load_professions()
+    for profession in professions:
+        if profession["title"].lower() == title.lower():
+            return profession
+    return "У нас нет такой профессии"
+
+
+def check_fitness(student: dict, profession: dict) -> dict:
+    """
+    Get student dict and profession dict return dictionary example:
+    {
+      "has": ["Python", "Linux"],
+      "lacks": ["Docker, SQL"],
+      "fit_percent": 50
+    }
+    :param student: dictionary with data about student
+    :param profession: dictionary with data about profession
+    :return: dictionary with suitable skills
+    """
+    has = set(student["skills"]).intersection(set(profession["skills"]))
+    lacks = set(profession["skills"]).difference(set(student["skills"]))
+    fit_percent = round(len(has) / len(profession["skills"]) * 100)
+    result_dict = {
+        "has": has,
+        "lacks": lacks,
+        "fit_percent": fit_percent
+    }
+    return result_dict
+
---
+++ SkyPro_Python/7_2/HomeWork/main.py
@@ -0,0 +1,10 @@
+from functions import get_student_by_pk, get_profession_by_title, check_fitness
+
+
+def main():
+    print('Введите номер студента')
+    pk = int(input())
+
+
+if __name__ == '__main__':
+    main()
