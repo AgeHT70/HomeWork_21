40c1a40e8ee95e86b33822e60e5a696a82d43c0f
ageht <Vladimir.E.Pavlov@yandex.ru>
2022-08-18 22:20:17 +0700

    utils: docstring for function load_random_word

--- SkyPro_Python/Course_work_2/utils.py
+++ SkyPro_Python/Course_work_2/utils.py
@@ -4,11 +4,13 @@ from basic_word import BasicWord
 
 
 def load_random_word(url: str) -> BasicWord:
+    """
+    Get json_file with list of words from URL
+    Choose random word from list, create an instance of the class `BasicWord`
+    return this instance
+    :param url: site with list of words
+    :return: instance of the class `BasicWord`
+    """
     data = requests.get(url).json()
     basic_word = BasicWord(*choice(data).values())
     return basic_word
-
-
-# - получит список слов с внешнего ресурса,
-# - выберет случайное слово, - создаст экземпляр класса `BasicWord`,
-# - вернет этот экземпляр.
